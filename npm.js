e0b22da810708805205a80766a07b22e1619dd24
munchkinhalfling <alex@rutins.com>
2018-12-22 14:56:39 -0500

    It's ali-brary!

--- npm.js
+++ npm.js
@@ -1,1 +1,14 @@
-
+((EasyXHR, MHPMScripts) => {
+  return {
+    load(pkgName) {
+      MHPMScripts.run("https://unpkg.com/@babel/standalone/babel.min.js"); // load Babel
+      let npmPkgSource = EasyXHR.getSync("https://cdn.jsdelivr.net/npm/" + pkgName);
+      let transformedPkgSrc = Babel.transform(npmPkgSource);
+      let require = this.load;
+      let module = {exports: {}};
+      let exports = new Proxy(module.exports, {});
+      eval(transformedPkgSrc);
+      return module.exports;
+    }
+  }
+})
