be1dd830e75b49312a54425f9122f8a61e940b01
munchkinhalfling <alex@rutins.com>
2018-12-22 17:47:06 -0500

    Switch to UNPKG

--- npm.js
+++ npm.js
@@ -2,7 +2,7 @@
   return {
     load(pkgName) {
       MHPMScripts.run("https://unpkg.com/@babel/standalone/babel.min.js"); // load Babel
-      let npmPkgSource = EasyXHR.getSync("https://cdn.jsdelivr.net/npm/" + pkgName);
+      let npmPkgSource = EasyXHR.getSync("https://unpkg.com/" + pkgName);
       let transformedPkgSrc = Babel.transform(npmPkgSource, {presets: ['es2015']}).code;
       let require = this.load;
       let module = {exports: {}};
